1a6baec12a73e324f8fe0aa4258f3c79e0104815
xQsme <pedro-ferreira92@hotmail.com>
2020-08-11 19:30:19 +0100

    mobile-nav

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,6 @@ import './assets/styles/main.scss';
 interface AppState {
 }
 
-const sections: any[] = [
-  {name: 'Section 0', id: 'section-0'},
-  {name: 'Section 1', id: 'section-1'},
-  {name: 'Section 2', id: 'section-2'},
-  {name: 'Section 3', id: 'section-3'},
-  {name: 'Section 4', id: 'section-4'},
-  {name: 'Section 5', id: 'section-5'}
-];
 
 class App extends Component <{}, AppState> {
   constructor(props: object) {
@@ -25,8 +17,8 @@ class App extends Component <{}, AppState> {
   render () {
     return(
       <div className="App" id="app">
-        <Nav sections={sections}/>
-        <Content sections={sections}/>
+        <Nav />
+        <Content />
       </div>
     );
   }
--- src/components/elements/Nav.tsx
+++ src/components/elements/Nav.tsx
@@ -1,23 +1,30 @@
 import React, {Component} from 'react';
+import About from '@material-ui/icons/AccountCircle';
+import Skills from '@material-ui/icons/Build';
+import Technologies from '@material-ui/icons/Settings';
+import Projects from '@material-ui/icons/Dashboard';
+import Contacts from '@material-ui/icons/Chat';
+import Left from '@material-ui/icons/ChevronLeft';
+import Right from '@material-ui/icons/ChevronRight';
+
 
 let mainNavLinks: any = [];
 let maybeMyElement: HTMLElement = document.getElementById('app') as HTMLElement;
 
 interface NavProps {
-    sections: any[],
 }
 
 interface NavState {
     currentSection: number,
-    sections: any[],
+    expanded: boolean,
 }
 
 class Nav extends Component <NavProps, NavState> {
   constructor(props: any) {
     super(props);
     this.state = {
-      sections: props.sections,
       currentSection: 0,
+      expanded: false,
     };
   }
 
@@ -26,7 +33,7 @@ class Nav extends Component <NavProps, NavState> {
   
     mainNavLinks.forEach((link: any, index: number) => {
       let section = document.querySelector(link.hash);
-      if (section.offsetTop <= (fromTop + 50)) {
+      if(section && section.offsetTop <= (fromTop + 50)) {
         this.setState({currentSection: index})
         return;
       }
@@ -47,24 +54,65 @@ class Nav extends Component <NavProps, NavState> {
     }, 500);
   }
 
+  toggleExpanded = () => {
+    const { expanded } = this.state;
+    this.setState({expanded: !expanded});
+    setTimeout(() => {
+      let fromTop = maybeMyElement.scrollTop;
+  
+      mainNavLinks.forEach((link: any, index: number) => {
+        let section = document.querySelector(link.hash);
+        if(section && section.offsetTop <= (fromTop + 50)) {
+          this.setState({currentSection: index})
+          return;
+        }
+      });
+    }, 300);
+  }
+
   render () {
-    const { currentSection, sections } = this.state;
+    const { currentSection, expanded } = this.state;
     return(
-    <div className="nav-container">
+    <div className={"nav-container" + (expanded ? '' : ' shrinked')}>
         <div className="nav">
-        <div className="nav-border-main" />
-        <div className="nav-border-selected" style={{marginTop: currentSection*30 + 'px'}} />
-        {sections.map((section: any, index: number) => {
-            return(
-            <div key={section.id} className={"nav-element" + (currentSection === index ? " active" : "")}>
-                <div className="nav-border"/>
-                <a href={"#" + section.id} onClick={() => this.scrollTo(index)}>
-                    <p>{section.name}</p>
-                </a>
-            </div>
-            );
-        })}
+          <div className="nav-border-main" />
+          <div className="nav-border-selected" style={{marginTop: currentSection*30 + 'px'}} />
+          <div className={"nav-element" + (currentSection === 0 ? " active" : "")}>
+              <div className="nav-border"/>
+              <a href="#about" onClick={() => this.scrollTo(0)}>
+                  <p className="nav-entry"><About /><span className="nav-word">About Me</span></p>
+              </a>
+          </div>
+          <div className={"nav-element" + (currentSection === 1 ? " active" : "")}>
+              <div className="nav-border"/>
+              <a href="#skills" onClick={() => this.scrollTo(1)}>
+              <p className="nav-entry"><Skills /><span className="nav-word">Skills</span></p>
+              </a>
+          </div>
+          <div className={"nav-element" + (currentSection === 2 ? " active" : "")}>
+              <div className="nav-border"/>
+              <a href="#technologies" onClick={() => this.scrollTo(2)}>
+              <p className="nav-entry"><Technologies /><span className="nav-word">Technologies</span></p>
+              </a>
+          </div>
+          <div className={"nav-element" + (currentSection === 3 ? " active" : "")}>
+              <div className="nav-border"/>
+              <a href="#projects" onClick={() => this.scrollTo(3)}>
+              <p className="nav-entry"><Projects /><span className="nav-word">Projects</span></p>
+              </a>
+          </div>
+          <div className={"nav-element" + (currentSection === 4 ? " active" : "")}>
+              <div className="nav-border"/>
+              <a href="#contacts" onClick={() => this.scrollTo(4)}>
+              <p className="nav-entry"><Contacts /><span className="nav-word">Contacts</span></p>
+              </a>
+          </div>
         </div>
+        {expanded ? (
+          <Left onClick={this.toggleExpanded} className="mobile-nav-expand" />
+        ) : (
+          <Right onClick={this.toggleExpanded} className="mobile-nav-expand" />
+        )}
     </div>
     );
   }
