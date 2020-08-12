bfd8a8a521564c3fe8465d6cc68a52c18407907e
xQsme <pedro-ferreira92@hotmail.com>
2020-08-12 15:22:14 +0100

    tech animations

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -19,9 +19,11 @@ import quarkus from '../assets/images/quarkus.png';
 import git from '../assets/images/git.png';
 
 let cards: any = [];
+let tech: any = [];
 let maybeMyElement: HTMLElement = document.getElementById('app') as HTMLElement;
 let lastScrollTop = 0;
 let lastScroll = Date.now();
+let animated = 0;
 
 interface ContentState {
     currentSkill: number,
@@ -39,46 +41,113 @@ class Content extends Component <ContentProps, ContentState> {
   }
 
   scrollFunc = (event: any) => {
-    if(maybeMyElement.offsetWidth <= 475){
         let time = Date.now();
-        if(lastScroll + 300 <= time) {
-          lastScroll = time;
+        if(lastScroll + 100 <= time) {
+            lastScroll = time;
             let fromTop = maybeMyElement.scrollTop;
             var st = window.pageYOffset || maybeMyElement.scrollTop;
-            if (st > lastScrollTop){
-                let found = false;
-                cards.forEach((card: any, index: number) => {
-                  if(card.offsetTop <= (fromTop + card.offsetHeight/3*2) && (card.offsetTop + card.offsetHeight/3) > fromTop) {
-                    this.setState({currentSkill: index})
-                    found = true;
-                    return;
-                  }
-                });
-                if(!found) {
-                    this.setState({currentSkill: -1})
-                }
-            } else {
-                let found = false;
-                cards.forEach((card: any, index: number) => {
-                  if(card.offsetTop <= (fromTop + card.offsetHeight) && (card.offsetTop + card.offsetHeight/3) > fromTop) {
-                    this.setState({currentSkill: index})
-                    found = true;
-                    return;
-                  }
-                });
-                if(!found) {
-                    this.setState({currentSkill: -1})
+            if(maybeMyElement.offsetWidth <= 475){
+                if (st > lastScrollTop){
+                    let found = false;
+                    cards.forEach((card: any, index: number) => {
+                    if(card.offsetTop <= (fromTop + card.offsetHeight/3*2) && (card.offsetTop + card.offsetHeight/3) > fromTop) {
+                        this.setState({currentSkill: index})
+                        found = true;
+                        return;
+                    }
+                    });
+                    if(!found) {
+                        this.setState({currentSkill: -1})
+                    }
+                } else {
+                    let found = false;
+                    cards.forEach((card: any, index: number) => {
+                    if(card.offsetTop <= (fromTop + card.offsetHeight) && (card.offsetTop + card.offsetHeight/3) > fromTop) {
+                        this.setState({currentSkill: index})
+                        found = true;
+                        return;
+                    }
+                    });
+                    if(!found) {
+                        this.setState({currentSkill: -1})
+                    }
                 }
             }
-            lastScrollTop = st <= 0 ? 0 : st; 
-        }
+            tech.forEach((t: HTMLElement) => {
+                if(!t.classList.contains('animated') && (t.offsetTop - maybeMyElement.offsetHeight) <= fromTop) {
+                    let right = false;
+                    if(maybeMyElement.offsetWidth > 1400) {
+                        if(animated < 4) {
+                            right = true;
+                        }
+                        //4 per row
+                    } else if(maybeMyElement.offsetWidth > 800) {
+                        if(animated < 3 || animated > 5) {
+                            right = true;
+                        }
+                        //3 per row
+                    } else if(maybeMyElement.offsetWidth > 475) {
+                        if(animated < 2 || animated > 3 || animated > 5) {
+                            right = true;
+                        }
+                        //2 per row
+                    } else {
+                        if(animated % 2 === 0) {
+                            right = true;
+                        }
+                        //1 per row
+                    }
+                    if(right) {
+                        t.classList.add('animate-right', 'animated');
+                    } else {
+                        t.classList.add('animate-left', 'animated');
+                    }
+                    animated++;
+                }
+            });
+
+        lastScrollTop = st <= 0 ? 0 : st;
     }
   }
 
   componentDidMount() {
     cards = document.querySelectorAll(".card-grid .card");
+    tech = document.querySelectorAll(".technologies-container .technology");
     maybeMyElement = document.getElementById('app') as HTMLElement;
     maybeMyElement.addEventListener("scroll", this.scrollFunc);
+    let fromTop = maybeMyElement.scrollTop;
+    tech.forEach((t: HTMLElement) => {
+        if(!t.classList.contains('animated') && (t.offsetTop - maybeMyElement.offsetHeight) <= fromTop) {
+            let right = false;
+            if(maybeMyElement.offsetWidth > 1400) {
+                if(animated < 4) {
+                    right = true;
+                }
+                //4 per row
+            } else if(maybeMyElement.offsetWidth > 800) {
+                if(animated < 3 || animated > 5) {
+                    right = true;
+                }
+                //3 per row
+            } else if(maybeMyElement.offsetWidth > 475) {
+                if(animated < 2 || animated > 3 || animated > 5) {
+                    right = true;
+                }
+                //2 per row
+            } else {
+                if(animated % 2 === 0) {
+                    right = true;
+                }
+                //1 per row
+            }
+            if(right) {
+                t.classList.add('animate-right', 'animated');
+            } else {
+                t.classList.add('animate-left', 'animated');
+            }
+            animated++;
+        }
+    });
   }
 
   render () {
@@ -140,35 +209,35 @@ class Content extends Component <ContentProps, ContentState> {
                     <h1><Technologies />Technologies</h1>
                 </div>
                 <div className="technologies-container">
-                    <div className="technology bounceInRight">
+                    <div className="technology enter">
                         <img src={react} alt="React.js" />
                         <h3>React.js</h3>
                     </div>
-                    <div className="technology bounceInRight">
+                    <div className="technology enter">
                         <img src={node} alt="Node.js" />
                         <h3>Node.js</h3>
                     </div>
-                    <div className="technology bounceInRight">
+                    <div className="technology enter">
                         <img src={quarkus} alt="Quarkus" />
                         <h3>Quarkus</h3>
                     </div>
-                    <div className="technology bounceInRight">
+                    <div className="technology enter">
                         <img src={qt} alt="Qt" />
                         <h3>Qt</h3>
                     </div>
-                    <div className="technology bounceInLeft">
+                    <div className="technology enter">
                         <img src={sass} alt="Sass" />
                         <h3>Sass</h3>
                     </div>
-                    <div className="technology bounceInLeft">
+                    <div className="technology enter">
                         <img src={git} alt="Git" />
                         <h3>Git</h3>
                     </div>
-                    <div className="technology bounceInLeft">
+                    <div className="technology enter">
                         <img src={aws} alt="AWS" />
                         <h3>AWS</h3>
                     </div>
-                    <div className="technology bounceInLeft">
+                    <div className="technology enter">
                         <img src={arch} alt="Linux" />
                         <h3>Linux</h3>
                     </div>
@@ -210,7 +279,6 @@ class Content extends Component <ContentProps, ContentState> {
                 Doloremque aliquam qui quisquam atque vero. Sed sequi inventore aut. Dolor cupiditate cupiditate voluptatem eligendi quo. Odit molestiae similique sapiente nulla possimus voluptas. Iste dolorem a non quia eaque temporibus rerum similique.
                 </p>
             </section>
-            <div className="content-spacer" />
         </div>
     );
   }
